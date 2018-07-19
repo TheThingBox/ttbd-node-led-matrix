4fba139dfc4577bc6d9e115a09c5b7dce2b7fe1b
lucaspouchot <lucas.pouchot@digitalairways.com>
2018-07-19 10:04:17 +0200

    check if font is available + apply auto offset on Y

--- 02-ledtext.js
+++ 02-ledtext.js
@@ -1,5 +1,15 @@
 module.exports = function(RED) {
   "use strict";
+  var fs = require("fs");
+  var path = require("path");
+
+  const ttbdNodeTextFont = "ttbd-node-text-font"
+  const ttbdNodeTextFontPath = path.dirname(require.resolve(`${ttbdNodeTextFont}/package.json`));
+  const fontPath = path.join(ttbdNodeTextFontPath, require(`${ttbdNodeTextFontPath}/package.json`).fontsLib)
+
+  const font = require(fontPath)
+  let fonts = []
+  font.get( f => { fonts = f })
 
   function ledtext(n) {
     RED.nodes.createNode(this,n);
@@ -8,6 +18,11 @@ module.exports = function(RED) {
     var node = this;
 
     this.on('input', function (msg) {
+
+      /************************************
+      ************** MESSAGE **************
+      ************************************/
+
       var message = node.message;
       if (message === null || typeof message === 'undefined' || message === "") {
         message = msg.message;
@@ -20,13 +35,81 @@ module.exports = function(RED) {
         }
       }
 
-      var textsize = null
-      if(msg.textsize){
+      if (typeof message === "object") {
+        try{
+          message = JSON.stringify(message);
+        } catch(e){
+          message = "[object Object]"
+        }
+      } else {
+        try{
+          message = message.toString()
+        } catch(e){
+          try{
+            message = message+""
+          } catch(e){
+            message = ""
+          }
+        }
+      }
+
+      /************************************
+      ************* FONT SIZE *************
+      ************************************/
+
+      var textsize = 3
+      if(msg.textsize && !isNaN(msg.textsize)){
         textsize = Number(parseInt(msg.textsize, 10) || 3)
         if(textsize>5) textsize = 5;
         if(textsize<1) textsize = 1;
-        textsize = 2*msg.textsize + 6
       }
+
+      textsize = 2*textsize + 6
+
+      /************************************
+      ************* TEXT FONT  ************
+      ************************************/
+
+      var textFont = `${msg.font || "Roboto-Regular"}`
+      var prefferedOffsetY = null
+
+      if(fonts.length > 0){
+        let indexFont = fonts.findIndex(f => {
+          let fontAvailable = []
+          for(var i in f.types){
+            fontAvailable.push(`${f.font.value}${f.types[i].value}`)
+          }
+          return fontAvailable.indexOf(textFont) !== -1
+        })
+        if(indexFont !== -1){
+          let size = fonts[indexFont].sizes.filter(s => s.size === `${textsize}`)
+          if(size.length > 0){
+            size = size[0]
+            prefferedOffsetY = size.offset
+          }
+        } else {
+          node.warn(`Unsupported font : ${textFont}`)
+          node.send(msg);
+          return;
+        }
+      }
+
+      /************************************
+      *************** OFFSET **************
+      ************************************/
+
+      var defaultOffsetX = 0
+      var defaultOffsetY = prefferedOffsetY || 0
+
+      var offset = {
+        x: (msg.offset && msg.offset.x)?msg.offset.x:defaultOffsetX,
+        y: (msg.offset && msg.offset.y)?msg.offset.y:defaultOffsetY
+      }
+
+
+      /************************************
+      *************** CHECK ***************
+      ************************************/
 
       if(typeof msg._led_matrix === "undefined") {
         msg._led_matrix = {
@@ -36,26 +119,15 @@ module.exports = function(RED) {
         msg._led_matrix.data = []
       }
 
-      if (typeof message === "object") {
-        message = JSON.stringify(message);
-      } else {
-        try{
-          message = message.toString()
-        } catch(e){
-          try{
-            message = message+""
-          } catch(e){}
-        }
-      }
+      /************************************
+      **************** SEND ***************
+      ************************************/
 
       msg._led_matrix.data.push({
         type: "str",
         content: message,
-        font: (msg.font||"Roboto-Regular") + "_" + (textsize||12),
-        offset: {
-          x: (msg.offset && msg.offset.x)?msg.offset.x:0,
-          y: (msg.offset && msg.offset.y)?msg.offset.y:0
-        },
+        font: `${textFont}_${textsize}`,
+        offset: offset,
         color: getColor(msg.color, msg.intensity, 'rgb', 'white', 40)
       })
 
